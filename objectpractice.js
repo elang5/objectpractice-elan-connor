667d667e0132898b2e914d70322ef24642505fdd
Connor Brock <brockconnor@yahoo.com>
2019-03-27 12:13:22 -0400

    drills

--- objectpractice.js
+++ objectpractice.js
@@ -23,3 +23,33 @@ const myObj = {
   name: 'Elan Green',
   city: 'Austin'
 };
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function(){
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  return person;
+}
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+function makeStudentReport(data){
+    data.forEach(student => {
+        return `${student.name}: ${student.grade}`
+    });
+}
